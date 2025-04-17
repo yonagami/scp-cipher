e95d2daf6070cb6e38a3c7aeb7448321ed67ed92
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-18 00:52:32 +0200

    Step 35

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for char in text.lower():
 		index = alphabet.find(char)
-		print(char, index)
 		new_index = index + shift
 		new_char = alphabet[new_index]
-		print(new_char)
+		print('char:',char, 'new char:',new_char)
