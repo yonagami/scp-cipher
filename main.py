e18ad11031f169d45983e67f369316d4a652fd95
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-20 22:08:58 +0200

    Step 45

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ for char in text.lower():
 				encrypted_text += char
 		else:
 				index = alphabet.find(char)
-				new_index = index + shift
+				new_index = (index + shift) % 26
 				encrypted_text += alphabet[new_index]
 		print('char:',char, 'encrypted text:',encrypted_text)
