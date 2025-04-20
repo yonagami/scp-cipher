534adcc8c823d57d2268e36ff62a103895e046a2
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-20 22:14:28 +0200

    Step 49

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 text = 'Hello Zaira'
 shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
-for char in text.lower():
-		if char == ' ':
-				encrypted_text += char
-		else:
-				index = alphabet.find(char)
-				new_index = (index + shift) % len(alphabet) 
-				encrypted_text += alphabet[new_index]
-print('plain text:',text)
-print('encrypted text:',encrypted_text)
+
+def caesar():
+
+		alphabet = 'abcdefghijklmnopqrstuvwxyz'
+		encrypted_text = ''
+		for char in text.lower():
+				if char == ' ':
+						encrypted_text += char
+				else:
+						index = alphabet.find(char)
+						new_index = (index + shift) % len(alphabet) 
+						encrypted_text += alphabet[new_index]
+		print('plain text:',text)
+		print('encrypted text:',encrypted_text)
