2e3105ec61c4eb46735fbd3e8a39489472cb400a
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-20 22:04:15 +0200

    Step 41

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encrypted_text = ''
 for char in text.lower():
-		print(char == ' ')
+		if char == ' ':
+				print('space!')
 		index = alphabet.find(char)
 		new_index = index + shift
 		encrypted_text += alphabet[new_index]
