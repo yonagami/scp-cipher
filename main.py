6376905b076cc147ba25d12f76d47aec040bfdc6
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-20 22:11:54 +0200

    Step 48

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ for char in text.lower():
 				index = alphabet.find(char)
 				new_index = (index + shift) % len(alphabet) 
 				encrypted_text += alphabet[new_index]
+print('plain text:',text)
 print('encrypted text:',encrypted_text)
