6cb83aa983daea177b5095a68c8d0cecc66d78ea
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-27 01:20:48 +0200

    Step 64

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def vigenere(message, key):
 				if char == ' ':
 						encrypted_text += char
 				else:
+            #Find the right key character to encode
 						key_char = key[key_index % len(key)]
 						key_index += 1
 						index = alphabet.find(char)
