9854ec26edeafe98192f58355bdb77103f861994
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-27 01:20:09 +0200

    Step 63

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def vigenere(message, key):
 						encrypted_text += char
 				else:
 						key_char = key[key_index % len(key)]
+						key_index += 1
 						index = alphabet.find(char)
 						new_index = (index + offset) % len(alphabet) 
 						encrypted_text += alphabet[new_index]
