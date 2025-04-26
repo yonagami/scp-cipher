03747dcffa74170e1a95e883db6d6e943aedb994
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-27 01:16:32 +0200

    Step 60

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ text = 'Hello Zaira'
 custom_key = 'python'
 
 def vigenere(message, key):
-
+		key_index = 0
 		alphabet = 'abcdefghijklmnopqrstuvwxyz'
 		encrypted_text = ''
 		for char in message.lower():
