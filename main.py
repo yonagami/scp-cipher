27f00062705e6f9efaae9d303997bb6a04155f5e
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-26 22:56:18 +0200

    Step 54

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ def caesar(message, offset):
 
 		alphabet = 'abcdefghijklmnopqrstuvwxyz'
 		encrypted_text = ''
-		for char in text.lower():
+		for char in message.lower():
 				if char == ' ':
 						encrypted_text += char
 				else:
 						index = alphabet.find(char)
-						new_index = (index + shift) % len(alphabet) 
+						new_index = (index + offset) % len(alphabet) 
 						encrypted_text += alphabet[new_index]
-		print('plain text:',text)
+		print('plain text:',message)
 		print('encrypted text:',encrypted_text)
 
 caesar()
