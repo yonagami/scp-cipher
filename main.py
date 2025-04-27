d9da51e752a1ff7b197e34c6e07898034e0496bb
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-28 00:25:50 +0200

    Step 71

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def vigenere(message, key, direction):
 						#Define the offset and the encrypted letter
 						offset = alphabet.index(key_char);
 						index = alphabet.find(char)
-						new_index = (index + offset) % len(alphabet) 
+						new_index = (index + offset * direction) % len(alphabet) 
 						encrypted_text += alphabet[new_index]
 		return encrypted_text
 
