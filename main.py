30cde7c4c0ab0eddac63763db6f2b31f277ec3cf
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-28 00:34:31 +0200

    Step 80

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def vigenere(message, key, direction = 1):
 		final_message = ''
 		for char in message.lower():
 				#Append space to the message
-				if char == ' ':
+				if char.isalpha():
 						final_message += char
 				else:
             #Find the right key character to encode/decode
