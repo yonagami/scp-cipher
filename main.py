8811888c94d2b71b3849ae359dc29379dc46f800
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-28 00:21:21 +0200

    Step 67

--- main.py
+++ main.py
@@ -18,6 +18,5 @@ def vigenere(message, key):
 						index = alphabet.find(char)
 						new_index = (index + offset) % len(alphabet) 
 						encrypted_text += alphabet[new_index]
-		print('plain text:',message)
-		print('encrypted text:',encrypted_text)
+		return encrypted_text
 
